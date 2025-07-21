6d1b0df39209c5e89d00ecf1eb13349a12457381
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-07-21 12:10:31 +0200

    2. GameScreen, drawing rectangle

--- src/main/java/org/casper/GameScreen.java
+++ src/main/java/org/casper/GameScreen.java
@@ -1,6 +1,16 @@
 package org.casper;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameScreen extends JPanel {
+    public GameScreen() { }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.RED);
+        g.fillRect(50, 50, 100, 100);
+
+    }
 }
