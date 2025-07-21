7fb5171e9affeee7ac299c8708baff4bbd69a23c
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-07-21 14:39:35 +0200

    3. Loading sprites

--- src/main/java/org/casper/Game.java
+++ src/main/java/org/casper/Game.java
@@ -1,21 +1,39 @@
 package org.casper;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame {
     private GameScreen gameScreen;
+    private BufferedImage img;
 
     public Game() {
-        setSize(400, 400);
+        importImg();
+
+        getContentPane().setPreferredSize(new Dimension(640, 640));
+        pack();
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+
+        gameScreen = new GameScreen(img);
         add(gameScreen);
     }
 
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Start of my TD Tutorial!");
         Game game = new Game();
 
 
--- src/main/java/org/casper/GameScreen.java
+++ src/main/java/org/casper/GameScreen.java
@@ -2,15 +2,53 @@ package org.casper;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameScreen extends JPanel {
-    public GameScreen() { }
+    private Random random;
+    private BufferedImage img;
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
+
+    public GameScreen(BufferedImage img) {
+        this.img = img;
+        loadSprites();
+
+        random = new Random();
+    }
+
+    private void loadSprites() {
+        for(int y = 0; y < 10; y++) {
+            for(int x = 0; x < 10; x++) {
+                BufferedImage sprite = img.getSubimage(32 * x, 32 * y, 32, 32);
+                sprites.add(sprite);
+            }
+        }
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.RED);
-        g.fillRect(50, 50, 100, 100);
+//        g.drawImage(sprites.get(19),0,0,null);
+//        g.drawImage(img.getSubimage(32 * 9, 32, 32, 32), 0, 0, null);
 
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.drawImage(sprites.get(getRandInt()), x * 32, y * 32, null);
+            }
+        }
+    }
+
+    private int getRandInt() {
+        return random.nextInt(100);
+    }
+
+    private Color getRandomColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
     }
 }
